b50a8d300cc06af8f0a8934993273c02aee33905
grash2024 <grash2024@gmail.com>
2025-09-29 11:35:15 +0530

    demo rider url added

--- src/data/url.js
+++ src/data/url.js
@@ -1,6 +1,3 @@
-// const url = "http://51.21.197.152:3000/api";
-//const url = "http://192.168.0.141:3000/api";
-// const url = "http://127.0.0.1:3000/api";
-// const url = "http://13.61.139.208:3000/api";
-const url = "https://mychits.online/api";
+
+const url = "http://4.240.96.223:3000/api/";
 export default url;
