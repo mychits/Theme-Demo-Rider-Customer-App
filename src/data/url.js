b68a430e56d169f358bc78e33d64d3ad4d3e64d2
NOOTHAN1608 <noothankp1608@gmail.com>
2025-11-14 15:55:45 +0530

    ready to final

--- src/data/url.js
+++ src/data/url.js
@@ -1,5 +1,5 @@
-// const url ="https://mychits.online/api";
-const url = "http://4.240.96.223:3000/api";
+const url ="https://mychits.online/api";
+// const url = "http://4.240.96.223:3000/api";
 export default url;
 
 
